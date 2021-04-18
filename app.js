d9e92b692b9ec84d87dad7c4a37d09c504b3146c
Fawaz Haroun <harounayomikun@gmail.com>
2021-04-18 20:40:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+const http = require("http");
+const { default: axios } = require("axios");
+
+const app = async () => {
+    const jsonFile = await JSON.stringify(axios.get("http://jsonplaceholder.typicode.com/posts"))
+
+    try {
+        fs.writeFile("./result/posts.json", (jsonFile), "utf-8", (err) => {
+            console.log("file written")
+        })
+        console.log(jsonFile)
+    } catch (err) {
+        console.log(`the error is ${err}`)
+    }
+}
+
+app()
